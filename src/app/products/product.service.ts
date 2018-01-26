b47756af9f8c9ec2483356fe38ffc6f5a1eb2e99
unknown <gustavoalzatesandoval@gmail.com>
2018-01-26 15:37:10 -0600

    Changes up to module 13, Demo: feature modules.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
@@ -19,6 +20,11 @@ constructor(private _http: HttpClient) {}
       return this._http.get<IProduct[]>(this._productUrl)
         .do(data => console.log('All: ' + JSON.stringify(data)))
         .catch(this.handleError);
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this.getProducts()
+        .map((products: IProduct[]) => products.find(p => p.productId === id));
   }
 
   private handleError(err: HttpErrorResponse) {
